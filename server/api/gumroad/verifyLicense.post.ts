d037826a8b12e4669312e83f8749b6f10223ca20
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-10-25 10:56:59 +0800

    feat: add the gumroad verify license api

---
+++ server/api/gumroad/verifyLicense.post.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export default defineEventHandler(async (event) => {
+  const { license_key } = await readBody(event)
+  let [res, message] = [null, null, null]
+
+  console.log(license_key)
+
+  const gumroadUrl = `https://api.gumroad.com/v2/licenses/verify?product_permalink=otg&license_key=${license_key}`
+
+  try {
+    res = await axios.post(gumroadUrl)
+  } catch (err) {
+    console.log(err)
+    message = err
+  }
+
+  return {
+    data: res.data,
+    error: !!message,
+    message
+  }
+})
