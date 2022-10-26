d199f422c005a81d724d4b37d9d66123148fc0ae
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-10-26 18:05:01 +0800

    feat: add the license upsert try catch

--- server/api/sdb/license/upsert.post.ts
+++ server/api/sdb/license/upsert.post.ts
@@ -2,17 +2,21 @@ import { sdb } from '@/utils/sdb'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-
-  let license = null
+  let [data, message] = [null, null]
 
   try {
     const res = await sdb.license.upsert(body)
     if (res && res.length === 1) {
-      license = res[0]
+      data = res[0]
     }
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log(err)
+    message = err
   }
 
-  return license
+  return {
+    data,
+    error: !!message,
+    message
+  }
 })
