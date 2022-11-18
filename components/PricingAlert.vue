83b0fcdf7899e5b33ac1c5226f0f99602c82bf73
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-11-18 11:34:05 +0800

    refactor: change the discount cout

--- components/PricingAlert.vue
+++ components/PricingAlert.vue
@@ -4,7 +4,7 @@
         class="container mx-auto flex justify-between w-2/3 2xl:w-1/2 text-white !bg-gradient-to-r from-shark-500 via-shark-700 to-shark-500 px-4 py-2 rounded-lg flex-between"
       >
         <main>
-          <p>🎉 Hey there! One Tab Group is now offering <span class="text-neon">50% discount</span> during the Black Friday period. Use code <span class="text-neon">"dgh1xvq"</span> to redeem this while checking out 😎</p>
+          <p>🎉 Hey there! One Tab Group is now offering <span class="text-neon">50% discount</span> during the Black Friday period. Use code <span class="text-neon">"msz2mwa"</span> to redeem this while checking out 😎</p>
         </main>
         <button
           @click="globalStore.showPricingAlert = false"
