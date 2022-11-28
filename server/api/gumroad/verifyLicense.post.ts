8f1b5a20dce25131fbfd26a5140deee222a6d9f4
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-11-28 18:14:19 +0800

    test: test the license api

--- server/api/gumroad/verifyLicense.post.ts
+++ server/api/gumroad/verifyLicense.post.ts
@@ -1,4 +1,5 @@
-import axios from '~/utils/http'
+// import http from '~/utils/http'
+// import axios from 'axios'
 
 export default defineEventHandler(async (event) => {
   const { license_key } = await readBody(event)
@@ -6,24 +7,25 @@ export default defineEventHandler(async (event) => {
 
   console.log(license_key)
 
-  // const gumroadUrl = `https://api.gumroad.com/v2/licenses/verify?product_permalink=otg&license_key=${license_key}`
+  const gumroadUrl = `https://api.gumroad.com/v2/licenses/verify?product_permalink=otg&license_key=${license_key}`
 
-  try {
-    res = await axios.post('https://api.gumroad.com/v2/licenses/verify', {
-      product_permalink: 'otg',
-      license_key
-    })
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-    message = err
-  }
+  // try {
+  //   res = await axios.post('https://api.gumroad.com/v2/licenses/verify', {
+  //     product_permalink: 'otg',
+  //     license_key
+  //   })
+  //   console.log(res)
+  // } catch (err) {
+  //   console.log(err)
+  //   message = err
+  // }
 
-  event.res.setHeader('Content-Type', 'json')
+  // event.res.setHeader('Content-Type', 'json')
+  res = await fetch(gumroadUrl)
 
   return {
     res,
-    data: res.data,
+    data: license_key,
     error: !!message,
     message
   }
