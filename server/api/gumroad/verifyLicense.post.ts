1019a161467a3d26ac4af992d7248ca807beed1a
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-12-12 15:17:24 +0800

    test: test the verify lifetime license features

--- server/api/gumroad/verifyLicense.post.ts
+++ server/api/gumroad/verifyLicense.post.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const { license_key } = await readBody(event)
-  let [res, message] = [null, null]
+  let [res, message] = [{ message: '', success: false }, null]
 
   // console.log(license_key)
   const response = await fetch(
@@ -15,6 +15,20 @@ export default defineEventHandler(async (event) => {
 
   res = await response.json()
   // console.log(res)
+
+  if (res !== null && !res?.success) {
+    const lifetime = await fetch(
+      `https://api.gumroad.com/v2/licenses/verify?product_permalink=otg_lifetime&license_key=${license_key}`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+
+    res = await lifetime.json()
+  }
 
   event.node.res.setHeader('Content-Type', 'application/json')
 
