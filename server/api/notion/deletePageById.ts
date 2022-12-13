0798a80718b99e62406ab82d342ffcc261db1f09
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-12-13 09:44:31 +0800

    test: test the notion page api

--- server/api/notion/deletePageById.ts
+++ server/api/notion/deletePageById.ts
@@ -2,13 +2,13 @@ import NotionConnector from '@/services/notion'
 
 export default defineEventHandler(async (event) => {
   const { token, pageId } = await readBody(event)
-  let data
-  let message
+  let data = null
+  let message = null
 
   const notion = new NotionConnector(token)
 
   try {
-    const res = notion.archivedNotionPageById(pageId)
+    const res = await notion.archivedNotionPageById(pageId)
     data = res
   } catch (error) {
     message = error
